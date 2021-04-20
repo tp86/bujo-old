44e95577b07e249c0837e1717ff52b21f04db9ca
tp86 <tom0palka+tp86@gmail.com>
2021-04-20 14:50:37 +0000

    Implement model along with definition

--- src/main/scala/bujo/domain/Model.scala
+++ src/main/scala/bujo/domain/Model.scala
@@ -1,14 +1,15 @@
 package bujo.domain
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 import java.time.LocalDateTime
+import cats.data.EitherT
+import cats.implicits.{catsSyntaxEither, catsStdInstancesForFuture}
 
 // Model entities
 final case class Tag(val name: String)
 
 opaque type NoteText = String
-private[domain] object NoteText:
-  def apply(text: String): NoteText = text
 
 final case class Note(text: NoteText, dateCreated: LocalDateTime, tags: Set[Tag])
 
@@ -20,13 +21,19 @@ class TextValidationError(private val msg: String) extends Error:
   val message = msg
 class NoteSavingError(private val msg: String) extends Error:
   val message = msg
-
-type ValidationError = TextValidationError
-
-type NoteCreationError = ValidationError | NoteSavingError
+enum NoteCreationError:
+  case ValidationError(err: TextValidationError)
+  case SavingError(err: NoteSavingError)
 
 // Procedures
-type NoteCreator = String => Either[Seq[NoteCreationError], Note]
-type TextValidator = String => Either[Seq[TextValidationError], NoteText]
-type NoteMaker = NoteText => Note
-type NoteSaver = Note => Future[Either[Seq[NoteSavingError], Note]]
+object NoteText:
+  def apply(text: String): NoteText = text
+def validateText(text: String): Either[TextValidationError, NoteText] = ???
+object Note:
+  def apply(text: NoteText): Note = new Note(text, LocalDateTime.now, Set.empty)
+def saveNote(note: Note): EitherT[Future, NoteSavingError, Note] = ???
+def createNote(text: String): EitherT[Future, NoteCreationError, Note] = 
+  val validatedNote = validateText(text) map Note.apply leftMap NoteCreationError.ValidationError.apply
+  EitherT.fromEither(validatedNote) flatMap {
+    note => saveNote(note) leftMap NoteCreationError.SavingError.apply
+  }
