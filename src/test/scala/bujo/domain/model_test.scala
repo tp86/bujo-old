c9b7f55f6e76681e8b57f21b911f9083ef25618b
tp86 <tom0palka+tp86@gmail.com>
2021-04-26 07:17:47 +0000

    Refactor test

--- src/test/scala/bujo/domain/model_test.scala
+++ src/test/scala/bujo/domain/model_test.scala
@@ -1,10 +1,8 @@
 package bujo.domain
 
 import scala.language.postfixOps
-import org.scalatest.{Outcome, Failed}
 import org.scalatest.flatspec
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{Success, Failure}
 
 import java.time.LocalDateTime
 
@@ -12,18 +10,21 @@ class CreateNote extends flatspec.FixtureAsyncFlatSpec:
 
   case class FixtureParam(text: String)
 
-  def withFixture(test: OneArgAsyncTest) = 
+  def withFixture(test: OneArgAsyncTest) =
     super.withFixture(test.toNoArgAsyncTest(FixtureParam("Some note text")))
 
   "a Note" should "be created given valid NoteText" in { f =>
     import bujo.domain.givens.saver
     createNote(f.text).value map { e =>
       e match
-        case Left(errs) => fail(errs.map(_.message).mkString("\n"))
-        case Right(note) => assert(note.isInstanceOf[Note])
+        case Left(errs)  => fail(errs.map(_.message).mkString("\n"))
+        case Right(note) => {
+          assert(note.isInstanceOf[Note])
+          assert(note.text equals "Some note text")
+        }
     }
   }
-  
+
   it should " be created with current timestamp" in { f =>
     import bujo.domain.givens.saver
     createNote(f.text).value map { e =>
@@ -35,7 +36,7 @@ class CreateNote extends flatspec.FixtureAsyncFlatSpec:
         }
     }
   }
-  
+
   it should "be created with empty Tag set" in { f =>
     import bujo.domain.givens.saver
     createNote(f.text).value map { e =>
@@ -46,3 +47,19 @@ class CreateNote extends flatspec.FixtureAsyncFlatSpec:
         }
     }
   }
+
+  it should "return a validation error when text is too long" in { _ =>
+    import bujo.domain.givens.saver
+    createNote("very long text ".repeat(18)).value map { e =>
+      e match
+        case Left(errs) =>
+          assert(
+            errs
+              .filter(e => e.isInstanceOf[NoteCreationError.ValidationError])
+              .size > 0
+          )
+        case Right(_) => fail("should return validation error")
+    }
+  }
+
+end CreateNote
