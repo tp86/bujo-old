04639c4bed33e1595181994aedd01ee86aaf28e4
tp86 <tom0palka+tp86@gmail.com>
2021-05-02 14:42:10 +0000

    WIP new scenario description

--- src/acceptance-test/scala/bujo/domain/acceptance.scala
+++ src/acceptance-test/scala/bujo/domain/acceptance.scala
@@ -9,34 +9,56 @@ import bujo.domain.validation.*
 class NoteFeatureTest extends AnyFeatureSpec, GivenWhenThen:
 
   Feature("Basic Note operations") {
-    Scenario("user creates note with valid text") {
-      Given("user provides valid text")
+    Scenario("User creates note with valid text") {
+      Given("User provides valid text")
       val text: String = "Some note text"
       assert(validateText(text).isRight)
-      
-      When("user creates note")
+
+      When("User creates note")
       val note = Note.create(text)
 
-      Then("note is created")
-      note match 
+      Then("Note is created")
+      note match
         case Right(n) =>
           assert(n.isInstanceOf[Note])
           assert(n.text equals text)
-        case Left(errs) => fail(s"failed with errors:\n\t${errs.map(_.message).mkString("\n\t")}")
+        case Left(errs) =>
+          fail(
+            s"Failed with errors:\n\t${errs.map(_.message).mkString("\n\t")}"
+          )
     }
-    
-    Scenario("user creates note with invalid text") {
-      Given("user provides invalid text")
+
+    Scenario("User creates note with invalid text") {
+      Given("User provides invalid text")
       val invalidText: String = "invalid text".repeat(22)
       assert(validateText(invalidText).isLeft)
-      
-      When("user creates note")
+
+      When("User creates note")
       val note = Note.create(invalidText)
 
-      Then("note text validation error is returned")
+      Then("Note text validation error is returned")
       note match
         case Left(errs) =>
           assert(errs exists (_.isInstanceOf[error.NoteTextValidationError]))
-        case Right(_) => fail("note should not be created with invalid text")
+        case Right(_) => fail("Note should not be created with invalid text")
+    }
+
+    Scenario("User searches for Notes matching text") {
+      Given("Existing Notes")
+      val notes = List("Some note text", "Interesting text")
+        .map(Note.create)
+        .map {
+          case Left(errs) =>
+            fail(
+              s"Failed with errors:\n\t${errs.map(_.message).mkString("\n\t")}"
+            )
+          case Right(note) => note
+        }
+      
+        When("User seaches for Notes using text")
+        pending
+        
+        Then("Matching Notes are returned")
+        pending
     }
   }
